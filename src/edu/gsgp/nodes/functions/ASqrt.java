9f741e8fa98e1ea01cc0339b7980ced0b4227319
Luiz Otavio V. B. Oliveira <luiz.vbo@gmail.com>
2017-09-18 17:58:01 +0100

    New analytic operators

---
+++ src/edu/gsgp/nodes/functions/ASqrt.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.gsgp.nodes.functions;
+
+import edu.gsgp.nodes.Node;
+
+/**
+ * Analytic Quotient
+ * @author Luiz Otavio Vilas Boas Oliveira
+ * http://homepages.dcc.ufmg.br/~luizvbo/ 
+ * luiz.vbo@gmail.com
+ * Copyright (C) 20014, Federal University of Minas Gerais, Belo Horizonte, Brazil
+ */
+public class ASqrt extends Function{
+    public ASqrt() { 
+        super();
+    }
+
+    @Override
+    public int getArity() { return 1; }
+    
+    @Override
+    public double eval(double[] inputs) {
+        double tmp = arguments[0].eval(inputs) * arguments[0].eval(inputs);
+        return Math.sqrt(Math.sqrt(1+tmp));
+    }
+
+    @Override
+    public int getNumNodes() {
+        return arguments[0].getNumNodes() + 1;
+    }
+    
+    @Override
+    public Function softClone() {
+        return new ASqrt();
+    }
+    
+    @Override
+    public String toString() {
+        return "ASqrt(" + arguments[0].toString() + ")";
+    }
+    
+    @Override
+    public Node clone(Node parent) {
+        ASqrt newNode = new ASqrt();
+        for(int i = 0; i < getArity(); i++) newNode.arguments[i] = arguments[i].clone(newNode);
+        newNode.parent = parent;
+        newNode.parentArgPosition = parentArgPosition;
+        return newNode;
+    }
+}
